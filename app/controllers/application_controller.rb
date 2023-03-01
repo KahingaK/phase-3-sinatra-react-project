69e2741567fc034f272388dccde06132e6dc66c5
IvanKahinga <ikahinga@gmail.com>
2023-03-01 18:04:04 +0300

    Initialized CRUD requests backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/projects" do
+
+    projects = Project.all
+    projects.to_json
+   
   end
+
+  delete "/projects/:id" do
+    project = Project.find(params[:id])
+
+    project.destroy
+
+    project.to_json
+  end
+
+  post "/projects" do
+    projects = Project.create()
+
+    projects.to_json
+  end
+
+  patch "/projects/:id" do
+    projects = Project.find(params[:id])
+    projects.update()
+  end
+
 
 end
